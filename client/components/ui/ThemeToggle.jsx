b3782218f53f7a900da8d0344cf9eb532dbbf6d2
Lara Alexander <lara.alexander@hotmail.com>
2024-06-17 15:20:33 -0400

    Adjust tab spacing

--- client/components/ui/ThemeToggle.jsx
+++ client/components/ui/ThemeToggle.jsx
@@ -4,39 +4,39 @@ import { useEffect } from 'react'
 import { useTheme } from 'next-themes'
 
 const ThemeToggle = () => {
-    const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
-    const changeTheme = (e) => {
-        if (e.target.checked) {
-            localStorage.setItem('100f-theme', 'dark')
-            setTheme('dark')
-        }
-        else {
-            localStorage.setItem('100f-theme', 'light')
-            setTheme('light')
-        }
+  const changeTheme = (e) => {
+    if (e.target.checked) {
+      localStorage.setItem('100f-theme', 'dark')
+      setTheme('dark')
     }
+    else {
+      localStorage.setItem('100f-theme', 'light')
+      setTheme('light')
+    }
+  }
 
-    useEffect(() => {
-        const themePref = localStorage.getItem('100f-theme')
-        if (themePref === 'light') setTheme('light')
-        else setTheme('dark')
-    }, [])
+  useEffect(() => {
+    const themePref = localStorage.getItem('100f-theme')
+    if (themePref === 'light') setTheme('light')
+    else setTheme('dark')
+  }, [])
 
-    return (
-        <>
-            <label className="flex items-center justify-between w-full">
-                Theme:
-                <input
-                    id="toggle"
-                    type="checkbox"
-                    className="toggle"
-                    checked={theme === 'dark'}
-                    onChange={(e) => changeTheme(e)}
-                />
-            </label>
-        </>
-    )
+  return (
+    <>
+      <label className="flex items-center justify-between w-full">
+        Theme:
+        <input
+          id="toggle"
+          type="checkbox"
+          className="toggle"
+          checked={theme === 'dark'}
+          onChange={(e) => changeTheme(e)}
+        />
+      </label>
+    </>
+  )
 }
 
 export default ThemeToggle
